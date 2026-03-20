9f155650ccad2e19aff6a9e5761ad2bb2376c8d7
Jyotsna Bedse <jyotsna.bedse02@gmail.com>
2026-03-20 17:44:21 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -55,7 +55,7 @@ SELECT
 	pn.prd_key AS product_number,
 	pn.prd_nm AS product_name,
 	pn.cat_id AS category_id,
-	pc.cat AS categoty,                                                                                                             
+	pc.cat AS category,                                                                                                             
 	pc.subcat AS subcategory,
 	pc.maintenance,
 	pn.prd_cost AS cost,
