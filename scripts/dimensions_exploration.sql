67d747ac6bfc240e398bdcd1b41eb51ead56816b
Jyotsna Bedse <jyotsna.bedse02@gmail.com>
2026-04-03 18:13:13 +0530

    Update dimensions_exploration.sql

--- scripts/dimensions_exploration.sql
+++ scripts/dimensions_exploration.sql
@@ -1,1 +1,10 @@
+-- Dimensions Exploration
+-- Explore All Countries our customers come from
 
+SELECT DISTINCT country FROM gold.dim_customers
+
+
+-- Explore All Product Categories "The Major Divisions"
+
+SELECT DISTINCT category,subcategory,product_name FROM gold.dim_products
+ORDER BY 1,2,3
