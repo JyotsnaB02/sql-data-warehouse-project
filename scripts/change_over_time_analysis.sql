4e9a378ad242d0548003a4e344f901f175c4ba4f
Jyotsna Bedse <jyotsna.bedse02@gmail.com>
2026-04-06 18:24:22 +0530

    Update change_over_time_analysis.sql

--- scripts/change_over_time_analysis.sql
+++ scripts/change_over_time_analysis.sql
@@ -1,1 +1,23 @@
+-- Analyse sales performance over time
 
+SELECT
+	DATETRUNC(month,order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month,order_date)
+ORDER BY DATETRUNC(month,order_date)
+
+/* With different date format */
+
+SELECT
+	FORMAT(order_date,'yyyy-MMM') AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date,'yyyy-MMM')
+ORDER BY FORMAT(order_date,'yyyy-MMM')
